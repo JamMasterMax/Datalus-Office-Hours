c2dd92588b80a71bec10f67cd13203dab51ceaa8
Maximillian A. Munguia <maximillian.munguia@gmail.com>
2016-02-26 15:32:53 -0800

    Create datalus.services.officeHours.js

---
+++ datalus.services.officeHours.js
@@ -0,0 +1,67 @@
+if (!datalus.services.officeHours) {
+    datalus.services.officeHours = {};
+}
+
+
+datalus.services.officeHours.getAll = function (onSuccess, onError) {
+    var url = "/api/officehours";
+    var settings = {
+        cache: false
+        , success: onSuccess
+        , error: onError
+        , type: "GET"
+    };
+    $.ajax(url, settings);
+}
+
+datalus.services.officeHours.getById = function (id, onSuccess, onError) {
+    var url = "/api/officehours/" + id;
+    var settings = {
+        cache: false
+        , success: onSuccess
+        , error: onError
+        , type: "GET"
+    };
+    $.ajax(url, settings);
+}
+
+datalus.services.officeHours.post = function (formData, onSuccess, onError) {
+    var url = "/api/officehours";
+    var settings = {
+        cache: false
+        , contentType: "application/json; charset=UTF-8"
+        , data: JSON.stringify(formData)
+        , dataType: "json"
+        , success: onSuccess
+        , error: onError
+        , type: "POST"
+    };
+    $.ajax(url, settings);
+}
+
+datalus.services.officeHours.put = function (id, formData, onSuccess, onError) {
+    var url = "/api/officehours/" + id;
+    var settings = {
+        cache: false
+        , contentType: "application/json; charset=UTF-8"
+        , data: JSON.stringify(formData)
+        , dataType: "json"
+        , success: onSuccess
+        , error: onError
+        , type: "PUT"
+    };
+    $.ajax(url, settings);
+}
+
+datalus.services.officeHours.delete = function (id, onSuccess, onError) {
+    var url = "/api/officehours/" + id;
+    var settings = {
+        cache: false
+        , contentType: "application/x-www-form-urlencoded; charset=UTF-8"
+        , dataType: "json"
+        , success: onSuccess
+        , error: onError
+        , type: "DELETE"
+    };
+    $.ajax(url, settings);
+}

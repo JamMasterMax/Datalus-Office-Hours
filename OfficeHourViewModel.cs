450c25821351b05695daa3f4d2ef397b33294b8b
Maximillian A. Munguia <maximillian.munguia@gmail.com>
2016-02-26 18:29:29 -0800

    Create OfficeHourViewModel.cs

---
+++ OfficeHourViewModel.cs
@@ -0,0 +1,15 @@
+namespace Datalus.Web.Models.ViewModels
+{
+
+    public class OfficeHourViewModel : BaseViewModel
+    {
+    
+        public int Id { get; set; }
+        
+        public int InstructorId { get; set; }
+        
+        public int SectionId { get; set; }
+        
+    }
+    
+}

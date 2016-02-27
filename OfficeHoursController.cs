86595683871b91866bd35f117121b738c4dd3a72
Maximillian A. Munguia <maximillian.munguia@gmail.com>
2016-02-26 17:30:32 -0800

    Create OfficeHoursController.cs

---
+++ OfficeHoursController.cs
@@ -0,0 +1,33 @@
+using Sabio.Web.Models.ViewModels;
+using System.Web.Mvc;
+
+
+namespace Sabio.Web.Controllers
+{
+
+    [RoutePrefix("OfficeHours")]
+    public class OfficeHoursController : BaseController
+    {
+    
+        [Route, HttpGet]
+        public ActionResult Index()
+        {
+            BaseViewModel model = new BaseViewModel();
+            model = DecorateViewModel(model);
+            return View(model);
+        }
+    
+        [Route("Create")] 
+        [Route("Create/{instructorId:int}/{sectionId:int}")]
+        [Route("{instructorId:int}/{sectionId:int}/{id:int}/Edit"), HttpGet]
+        public ActionResult Create(int id = 0, int instructorId = 0, int sectionId = 0)
+        {
+            OfficeHourViewModel model = new OfficeHourViewModel();
+            model = DecorateViewModel(model);
+            model.Id = id;
+            model.InstructorId = instructorId;
+            model.SectionId = sectionId;
+            return View(model);
+        }
+    }
+}
